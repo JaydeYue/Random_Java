68a259fd06e8fe7d11e06c3c97d27c801de94c20
Jayde Yue <JaydeYue@jaydeyue.com>
2018-03-11 20:27:26 -0400

    difference between %c and %s

--- Null in String/nullinstring.c
+++ Null in String/nullinstring.c
@@ -5,7 +5,9 @@ int main() {
     char string[] = "012xy\08s34f4w2";
     int i, n= 0;
     for (i=0;i<strlen(string); i++) {
-        printf("%s\n", string[i]);
+        // %c prints a character with sizeof(char)
+        // %s prints to the pointer to a null terminated string sizeof(char*)
+        printf("%c\n", string[i]);
     }
     return 0;
 }
