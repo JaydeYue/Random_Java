b86c529bc18a2230096c69c18b3b01933d4e6346
Jayde Yue <JaydeYue@jaydeyue.com>
2018-03-11 20:20:23 -0400

    fix single quotes are for single characters only

--- Null in String/nullinstring.c
+++ Null in String/nullinstring.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
-    char string[] = '012xy\08s34f4w2';
+    // Single quotes are for single characters
+    char string[] = "012xy\08s34f4w2";
     int i, n= 0;
     for (i=0;i<strlen(string); i++) {
         printf("%s\n", string[i]);
