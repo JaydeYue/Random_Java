6b52b095ffd5dfcba1422bc245945a412bd63dca
Jayde Yue <JaydeYue@jaydeyue.com>
2018-03-11 20:30:10 -0400

    test

--- Null in String/nullinstring.c
+++ Null in String/nullinstring.c
@@ -5,7 +5,8 @@ int main() {
     // Single quotes are for single characters
     char string[] = "012xy\08s34f4w2";
     int i, n= 0;
-    printf("Length of string: %d\n", strlen(string));
+    // Use %zu for size_t while %d is for int only
+    printf("Length of string: %zu\n", strlen(string));
     for (i=0;i<strlen(string); i++) {
         // %c prints a character with sizeof(char)
         // %s prints to the pointer to a null terminated string sizeof(char*)
